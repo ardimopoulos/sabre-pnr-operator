6ee23ad65962573155f3cce5f1a7ec077bd8650b
ardimopoulos <ardimopoulos@gmail.com>
2019-03-09 02:52:16 +0200

    Initial implementation of SessionCreate

--- src/main/java/com/sabre/pnrretriever/PnrRetrieverApplication.java
+++ src/main/java/com/sabre/pnrretriever/PnrRetrieverApplication.java
@@ -2,16 +2,13 @@ package com.sabre.pnrretriever;
 
 import com.sabre.pnrretriever.config.properties.HeaderProperties;
 import com.sabre.pnrretriever.handlers.SessionCreateHandler;
-import com.sabre.pnrretriever.headers.message_header.Action;
-import com.sabre.pnrretriever.responses.Response;
 import com.sabre.pnrretriever.rest.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PnrRetrieverApplication implements CommandLineRunner {
+public class PnrRetrieverApplication {
 
 	@Autowired
 	private SessionCreateHandler sessionCreateHandler;
@@ -26,16 +23,5 @@ public class PnrRetrieverApplication implements CommandLineRunner {
 		SpringApplication.run(PnrRetrieverApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-        System.out.println(Action.SESSION_CREATE.getValue());
-		Response sessionCreateResponse = sessionCreateHandler.processRequest();
-		System.out.println(sessionCreateResponse);
-
-		apiResponse.getApiResponse().put("session-create", sessionCreateResponse);
-		apiResponse.getApiResponse().put("sabre-command", sessionCreateResponse);
-		apiResponse.getApiResponse().put("session-close", sessionCreateResponse);
-//		apiResponse.setSessionCreateResponse(sessionCreateResponse);
-	}
 
 }
